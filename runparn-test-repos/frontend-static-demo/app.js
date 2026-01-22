98ca1d038a5d1c734c097687d0e5caeb2fc47c9f
Pongkrit <92305182+Pongkrit@users.noreply.github.com>
2026-01-22 14:22:51 +0700

    Delete runparn-test-repos directory

--- runparn-test-repos/frontend-static-demo/app.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById('btn');
-const out = document.getElementById('out');
-
-btn.addEventListener('click', () => {
-  const t = new Date().toLocaleString();
-  out.textContent = `clicked at ${t}`;
-});
